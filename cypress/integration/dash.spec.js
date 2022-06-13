89e1a0bc34b1c402739de6bf6a1943401b88af0a
qafrazz <francisco.frazzon@outlook.com>
2022-06-13 18:45:02 -0300

    tratamento das horas

---
+++ cypress/integration/dash.spec.js
@@ -0,0 +1,27 @@
+import dashPage from "../support/pages/dash"
+import { customer, provider, appointment } from '../support/factories/dash'
+
+describe('dashboard', () => {
+  context('quando o cliente faz um agendamento no app mobile', function () {
+
+    before(function () {
+      cy.postUser(provider)
+      cy.postUser(customer)
+      cy.apiLogin(customer)
+      //cy.log('conseguimos pegar o token ' + Cypress.env('apiToken'))
+      cy.setProviderId(provider.email)
+      cy.createAppointment(appointment.hour)
+    })
+
+
+    it('o mesmo deve ser exibido no dashbord', () => {
+      const date = Cypress.env('appointmentDate')
+
+      cy.apiLogin(provider, true)
+
+      dashPage.calendarShouldBeVisible()
+      dashPage.selectDay(date)
+      dashPage.appointmentShouldBe(customer, appointment.hour)
+    });
+  })
+});
--- cypress/support/pages/dash/index.js
+++ cypress/support/pages/dash/index.js
@@ -1,8 +1,77 @@
 import header from '../../components/header'
+import { el } from './elements'
 
 class DashPage {
   constructor() {
     this.header = header
+  }
+  calendarShouldBeVisible() {
+    cy.get(el.calendar, { timeout: 7000 })
+      .should('be.visible')
+  }
+
+  selectDay(appointmentDate) {
+    let today = new Date()
+    let lastDayOfMonth = new Date(today.getFullYear(), today.getMonth() + 1, 0)
+
+    if (today.getDate() === lastDayOfMonth.getDate()) {
+      cy.get(el.nextMonthButton)
+        .should('be.visible').click()
+      cy.log(appointmentDate.getMonth())
+      let monthName
+      switch (appointmentDate.getMonth()) {
+        case 0:
+          monthName = 'Janeiro'
+          break;
+        case 1:
+          monthName = 'Fevereiro'
+          break;
+        case 2:
+          monthName = 'Março'
+          break;
+        case 3:
+          monthName = 'Abril'
+          break;
+        case 4:
+          monthName = 'Maio'
+          break;
+        case 5:
+          monthName = 'Junho'
+          break;
+        case 6:
+          monthName = 'Julho'
+          break;
+        case 7:
+          monthName = 'Agosto'
+          break;
+        case 8:
+          monthName = 'Setembro'
+          break;
+        case 9:
+          monthName = 'Outubro'
+          break;
+        case 10:
+          monthName = 'Novembro'
+          break;
+        case 11:
+          monthName = 'Dezembro'
+          break;
+      }
+      cy.contains(el.monthYearName, monthName)
+        .should('be.visible')
+
+    }
+
+    const target = new RegExp(`^${appointmentDate.getDate()}$`, 'g')
+    cy.contains(el.boxDay, target)
+      .click({ force: true })
+  }
+  appointmentShouldBe(customer, hour) {
+    cy.contains('div', customer.name)
+      .should('be.visible')
+      .parent()
+      .contains(el.boxHour, hour)
+      .should('be.visible')
   }
 }
 
